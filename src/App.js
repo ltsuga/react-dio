cceb727645cfdb3cffebda968176ea6fe86d5e30
ltsuga <85001056+ltsuga@users.noreply.github.com>
2021-07-20 21:40:56 -0300

    Working with Components- React JS
    DIO Course lead

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Item from './components/item';
+import Card from './components/card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+  <>
+  <h1>Aplicação com React</h1>
+  <ul>
+  
+  <Item texto='item1' />
+  <Item />
+  <Item />
+  
+  
+  </ul>
+  <Card />
+  </>
+  )
 }
 
 export default App;
